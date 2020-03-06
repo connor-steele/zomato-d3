f9c169c8033fb51769daf93aafe1e9d9c00fd521
chromewhip <connorsteele604@gmail.com>
2020-03-06 06:21:44 -0800

    initial project structure

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+// const inventoryRoute = require("./routes/api/inventory");
+// app.use("/api/inventory", inventoryRoute);
+
+// const warehousesRoute = require("./routes/api/warehouses");
+// app.use("/api/warehouses", warehousesRoute);
+
+app.listen(5000, function() {
+  console.log("server is running on port 5000");
+});

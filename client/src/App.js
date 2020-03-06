24bc2217c80949aa7fa9b8be9f9d59d7b96a3d1c
fitale <fitalewari@gmail.com>
2020-03-06 12:56:10 -0800

    events

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./styles/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
 import Home from "./components/Home.js";
 import Profile from "./components/Profile.js";
 
 function App() {
   return (
     <Router className="App">
-      {/* <div className="App"></div> */}
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/profile" component={Profile} />
--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
-
 export default class extends Component {
   render() {
     return (
-      <div>
-        <h3>Events</h3>
-        <div>
-          <h5>Event Name</h5>
-        </div>
-      </div>
+      <section className="events">
+        <h3 className="events__title">Events</h3>
+        <section className="events__container">
+          <div className="events__container--flex margin-right">
+            <div className="left">
+              <h5 className="left__title">Event Name</h5>
+              <p className="left__description">Event Description</p>
+              <p className="left__description">Time</p>
+              <button className="left__button">
+                <p className="left__button--text">Book Now</p>
+              </button>
+            </div>
+            <div className="right">
+              <h5 className="right__calendar">Mar 6th</h5>
+            </div>
+          </div>
+          <div className="events__container--flex margin-left">
+            <div className="left">
+              <h5 className="left__title">Event Name</h5>
+              <p className="left__description">Event Description</p>
+              <p className="left__description">Time</p>
+
+              <button className="left__button">
+                <p className="left__button--text">Book Now</p>
+              </button>
+            </div>
+            <div className="right">
+              <h5 className="right__calendar">Mar 6th</h5>
+            </div>
+          </div>
+        </section>
+      </section>
     );
   }
 }

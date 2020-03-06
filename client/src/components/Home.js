815cccbdcb67e0f31c0cf0de4a49d96702f62b31
chromewhip <connorsteele604@gmail.com>
2020-03-06 13:47:53 -0800

    finished offers responsive

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Events from "./Events";
-
+import Offers from "./Offers";
 import Influencers from "./Influencers";
 export default class Home extends Component {
   render() {
@@ -10,6 +10,7 @@ export default class Home extends Component {
         <Header />
         <Influencers />
         <Events />
+        <Offers />
       </div>
     );
   }

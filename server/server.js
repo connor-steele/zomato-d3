c6ef67d98ceb2b2b3208b4516b2034f5d844790b
chromewhip <connorsteele604@gmail.com>
2020-03-06 06:56:56 -0800

    working python script within express

--- server/server.js
+++ server/server.js
@@ -1,16 +1,42 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-app.use(cors());
+// import express JS module into app
+// and creates its variable.
+var express = require("express");
+var app = express();
 
-app.use(express.json());
-
-// const inventoryRoute = require("./routes/api/inventory");
-// app.use("/api/inventory", inventoryRoute);
-
-// const warehousesRoute = require("./routes/api/warehouses");
-// app.use("/api/warehouses", warehousesRoute);
-
-app.listen(5000, function() {
-  console.log("server is running on port 5000");
+// Creates a server which runs on port 3000 and
+// can be accessed through localhost:3000
+app.listen(3000, function() {
+  console.log("server running on port 3000");
 });
+
+// Function callName() is executed whenever
+// url is of the form localhost:3000/name
+app.get("/name", callName);
+
+function callName(req, res) {
+  // Use child_process.spawn method from
+  // child_process module and assign it
+  // to variable spawn
+  var spawn = require("child_process").spawn;
+
+  // Parameters passed in spawn -
+  // 1. type_of_script
+  // 2. list containing Path of the script
+  //    and arguments for the script
+
+  // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+  // so, first name = Mike and last name = Will
+  var process = spawn("python", [
+    "./python/hello.py",
+    req.query.firstname,
+    req.query.lastname
+  ]);
+
+  // Takes stdout data from script which executed
+  // with arguments and send this data to res object
+  process.stdout.on("data", function(data) {
+    res.send(data.toString());
+  });
+}
+
+// save code as start.js
